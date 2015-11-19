47025fbd2ae44846334151a3df2c7c8fda9bd789
Hassanmir92 <hassanmir92@gmail.com>
2015-11-19 12:01:44 +0000

    data returned from ajax request save data.token in our localStorage

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -11,11 +11,24 @@ function submitForm(){
   var url    = "http://localhost:3000/api"+ $(this).attr("action");
   var data   = $(this).serialize();
 
+  return ajaxRequest(method, url, data)
+}
+
+function authenticationSuccessful(data) {
+  if (data.token) setToken(data.token);
+}
+
+function setToken(token) {
+  return window.localStorage.setItem("token", token);
+}
+
+function ajaxRequest(method, url, data, callback) {
   $.ajax({
     method: method,
     url: url,
     data: data
   }).done(function(data){
-    console.log(data)
+    console.log(data);
+    authenticationSuccessful(data);
   });
 }
